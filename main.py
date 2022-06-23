ff8208ef7688c86e7dc480c7b4b1f3a94e46bbd6
Malhar Patel <51440410+Unknown212-cyber@users.noreply.github.com>
2022-06-23 06:06:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# import colorgram
+#
+# colors = colorgram.extract('image.jpg', 30)
+# rgb_color = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_color.append(new_color)
+#
+# print(rgb_color)
+import turtle as t
+import random
+screen = t.Screen()
+screen.colormode(255)
+
+color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+tim = t.Turtle()
+tim.hideturtle()
+tim.penup()
+tim.setposition(-200, -200)
+y = -200
+tim.speed("fastest")
+for _ in range(100):
+    if _ % 10 == 0:
+        tim.setposition(-200, y)
+        y += 50
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+screen.exitonclick()
